1e38bc4adea54a0e7a16323cbf3c63e5a505c648
Peter-Kibet <peter.kibet@hotmail.com>
2023-06-07 05:04:02 +0300

    Refactor book and collection classes to manage book collection
    -Created a Book class to represent a book with properties for title and author.
    -The Collection class was refactored to manage a collection of books.
    -Initialized the books array in the Collection constructor and added a method to load books from local storage if available.
    -Added a method to the Collection class to update the local storage with the current books array.
    -Implemented an addBook method in the Collection class to add a new book to the collection, creating a new Book instance and updating the local storage.
    -Implemented a removeBook method in the Collection class to remove a book from the collection based on the provided index, updating the local storage.

--- script.js
+++ script.js
@@ -1,78 +1,61 @@
-// Array to store the books
-let books = [];
-
-// Retrieve stored books from local storage
-const storedBooks = localStorage.getItem('books');
-if (storedBooks) {
-  books = JSON.parse(storedBooks);
-}
-
-/**
- * Function to update the stored books in local storage.
- */
-function updateLocalStorage() {
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-/**
- * Function to display the books in the UI.
- */
-function displayBooks() {
+class Book {
   /**
-   * Function to remove a book from the collection.
+   * Creates a new instance of the Book class.
+   * @param {string} title - The title of the book.
+   * @param {string} author - The author of the book.
+   */
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+class Collection {
+  /**
+   * Creates a new instance of the Collection class.
+   * Initializes the books array and loads books from local storage if available.
+   */
+  constructor() {
+    this.books = [];
+    this.loadBooks();
+  }
+
+  /**
+   * Loads the books from local storage and populates the books array.
+   */
+  loadBooks() {
+    const storedBooks = localStorage.getItem("books");
+    if (storedBooks) {
+      this.books = JSON.parse(storedBooks).map(
+        ({ title, author }) => new Book(title, author)
+      );
+    }
+  }
+
+  /**
+   * Updates the local storage with the current books array.
+   */
+  updateLocalStorage() {
+    localStorage.setItem("books", JSON.stringify(this.books));
+  }
+
+  /**
+   * Adds a new book to the collection.
+   * @param {string} title - The title of the book.
+   * @param {string} author - The author of the book.
+   */
+  addBook(title, author) {
+    const newBook = new Book(title, author);
+    this.books.push(newBook);
+    this.updateLocalStorage();
+  }
+
+  /**
+   * Removes a book from the collection.
    * @param {number} index - The index of the book to remove.
    */
-  function removeBook(index) {
-    books.splice(index, 1);
-    displayBooks();
-    updateLocalStorage();
-  }
-
-  const booksDiv = document.getElementById('books');
-  booksDiv.innerHTML = '';
-
-  if (books.length === 0) {
-    booksDiv.innerHTML = '<p>No books found.</p>';
-  } else {
-    const ul = document.createElement('ul');
-
-    books.forEach((book, index) => {
-      const li = document.createElement('li');
-      li.textContent = `${book.title} by ${book.author}`;
-
-      const removeBtn = document.createElement('button');
-      removeBtn.type = 'button';
-      removeBtn.textContent = 'Remove';
-      removeBtn.addEventListener('click', () => {
-        removeBook(index);
-      });
-
-      li.appendChild(removeBtn);
-      ul.appendChild(li);
-    });
-
-    booksDiv.appendChild(ul);
+  removeBook(index) {
+    this.books.splice(index, 1);
+    this.updateLocalStorage();
   }
 }
-
-/**
- * Function to add a new book to the collection.
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
- */
-function addBook(title, author) {
-  const book = { title, author };
-  books.push(book);
-  displayBooks();
-  updateLocalStorage();
-}
-
-// Event listener for add button
-document.getElementById('addButton').addEventListener('click', () => {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  addBook(title, author);
-});
-
-// Initial display of books
-displayBooks();

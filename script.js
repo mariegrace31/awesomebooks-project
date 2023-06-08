6baea61f5c9c155ddc830048dfd9d490c47ac139
Peter-Kibet <peter.kibet@hotmail.com>
2023-06-08 16:21:20 +0300

    Add arrow function to retrieve the books from local storage

--- script.js
+++ script.js
@@ -1,89 +1,100 @@
-//Creating a class called BooksCollection to manage the books
-class BooksCollection {
+// Create a class called BookCollection to manage the books
+class BookCollection {
+  // The constructor is called when creating an instance of the class
+  constructor() {
+    // Initialize the books array by retrieving the stored books from local storage
+    this.books = this.retrieveBooksFromLocalStorage();
+    // Display the books in the UI
+    this.displayBooks();
+  }
 
-    constructor() {
-        this.books = this.getBooksFromLocalStorage();
-        //Display the books in the UI
-        this.displayBooks();
-    }
+  // Arrow function to retrieve the books from local storage
+  retrieveBooksFromLocalStorage = () => {
+    // Retrieve the stored books from local storage
+    const storedBooks = localStorage.getItem('books');
+    return storedBooks ? JSON.parse(storedBooks) : [];
+  };
 
-    //Method to retrieve the books from the local storage
-    retrieveBooksFromLocalStorage() {
-      //Retrieve the books from local storage
-      const storedBooks = localStorage.getItem('books');
-      //if there are stored books, parse to JSON and return them as an array, otherwise return an empty array
-      return storedBooks ? JSON.parse(storedBooks) : [];
-    }
+  // Method to update the stored books in local storage
+  updateLocalStorage() {
+    // Convert the books array to JSON and store it in the 'books' key of local storage
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
 
+  // Method to display the books in the UI
+  displayBooks() {
+    // Get the DOM element for the books container
+    const booksDiv = document.getElementById('books');
+    // Clear the books container
+    booksDiv.innerHTML = '';
 
-    //Method to update the stored books in the local storage
-    updateLocalStorage() {
-      //Converts the books array to a JSON string and stores it in the 'books' key of local storage
-      localStorage.setItem('books', JSON.stringify(this.books));
-    }
+    // Check if there are any books
+    if (this.books.length === 0) {
+      // If there are no books, display a message indicating so
+      booksDiv.innerHTML = '<p>No books found.</p>';
+    } else {
+      // If there are books, create an unordered list element
+      const ul = document.createElement('ul');
 
-    //Method to display the books in the UI
-    displayBooks() {
-      //Getting the DOM element for the books container
-      const booksDiv = document.getElementById('books');
-      //Clearing the books container
-      booksDiv.innerHTML = '';
+      // Iterate over each book in the books array
+      this.books.forEach((book, index) => {
+        // Create a list item element to display the book details
+        const li = document.createElement('li');
+        li.textContent = `${book.title} by ${book.author}`;
 
-      //Check if there are any books in the array
-      if (this.books.length === 0) {
-        //If there are no books, display a message indicating so.
-        booksDiv.innerHTML = '<p>No Books Found!</p>';
-      } else {
-        //if there are books, create an unordered list element
-        const ul = document.createElement('ul');
+        // Create a remove button for each book
+        const removeBtn = document.createElement('button');
+        removeBtn.type = 'button';
+        removeBtn.textContent = 'Remove';
 
-        //Iterate over each book in the array
-        this.books.forEach((book, index) =>{ 
-          //Create a list item element to display the book details
-          const li = document.createElement('li');
-          li.textContent = `${book.title} by ${book.author}`;
-
-          //Create a remove button for each book
-          const removeButton = document.createElement('button');
-          removeButton.type = 'button';
-          removeButton.textContent = 'Remove';
-
-          // Add an event listener to the remove button to handle book removal
-          removeButton.addEventListener('click', () => {
-            this.removeBook (index);
-          });
-          li.appendChild(removeButton);
-          ul.appendChild (li);
+        // Add an event listener to the remove button to handle book removal
+        removeBtn.addEventListener('click', () => {
+          // Call the removeBook method, passing the index of the book to remove
+          this.removeBook(index);
         });
-        booksDiv.appendChild (ul); 
-      }
-    }
-    // A method to add a new book to the collection
-    addBook(title, author){
-      // create a book object with a given title and author
-      const book = {
-        title, author
-      };
-      this.books.push (book);
-      this.displayBooks ();
-      this.updateLocalStorage();
-    }
 
-  // create a method to remove the book from the collection
-  removeBook(index){
-    this.books.splice (index, 1);
-    this.displayBooks ();
+        // Append the remove button to the list item
+        li.appendChild(removeBtn);
+        // Append the list item to the unordered list
+        ul.appendChild(li);
+      });
+
+      // Append the unordered list to the books container
+      booksDiv.appendChild(ul);
+    }
+  }
+
+  // Method to add a new book to the collection
+  addBook(title, author) {
+    // Create a book object with the given title and author
+    const book = { title, author };
+    // Add the book object to the books array
+    this.books.push(book);
+    // Display the updated list of books in the UI
+    this.displayBooks();
+    // Update the stored books in local storage
+    this.updateLocalStorage();
+  }
+
+  // Method to remove a book from the collection
+  removeBook(index) {
+    // Remove the book at the specified index from the books array
+    this.books.splice(index, 1);
+    // Display the updated list of books in the UI
+    this.displayBooks();
+    // Update the stored books in local storage
     this.updateLocalStorage();
   }
 }
 
-// create an instance of the bookCollection class to manage the books
-const bookCollection = new BooksCollection();
+// Create an instance of the BookCollection class to manage the books
+const bookCollection = new BookCollection();
+
 // Add an event listener to the add button
-document.getElementById('addButton').addEventListener('click', () =>{
-  const title = document.getElementById ('title').value;
-  const author = document.getElementById ('auhtor').value;
+document.getElementById('addButton').addEventListener('click', () => {
+  // Get the values of the title and author inputs
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  // Call the addBook method of the bookCollection instance, passing the title and author
   bookCollection.addBook(title, author);
-})
-
-
+});
